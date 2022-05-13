05490b502b32d0871c0571da764a810a09792da0
Takuro Ashie <ashie@clear-code.com>
2022-05-13 17:37:56 +0900

    Disable selection on long tap, double tap and triple tap
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- third_party/blink/renderer/core/input/gesture_manager.cc
+++ third_party/blink/renderer/core/input/gesture_manager.cc
@@ -396,6 +396,7 @@ WebInputEventResult GestureManager::HandleGestureShortPress(
 
 WebInputEventResult GestureManager::HandleGestureLongPress(
     const GestureEventWithHitTestResults& targeted_event) {
+#if 0
   const WebGestureEvent& gesture_event = targeted_event.Event();
 
   // FIXME: Ideally we should try to remove the extra mouse-specific hit-tests
@@ -448,6 +449,9 @@ WebInputEventResult GestureManager::HandleGestureLongPress(
       inner_node ? inner_node->GetDocument().GetFrame() : nullptr,
       mojom::blink::UserActivationNotificationType::kInteraction);
   return SendContextMenuEventForGesture(targeted_event);
+#else
+  return WebInputEventResult::kNotHandled;
+#endif
 }
 
 WebInputEventResult GestureManager::HandleGestureLongTap(
