13e126cdf7ce733fe71446771a177342c42e23b5
Takuro Ashie <ashie@clear-code.com>
2022-06-27 18:36:56 +0900

    [ozone/wayland] Fix build error of ui::views::LinuxUI
    ```
    ld.lld: error: undefined symbol: ui::ShellDialogLinux::SetInstance(ui::ShellDialogLinux*)
    >>> referenced by linux_ui.cc:25 (../../ui/views/linux_ui/linux_ui.cc:25)
    >>>               obj/ui/views/views/linux_ui.o:(views::LinuxUI::SetInstance(std::__Cr::unique_ptr<views::LinuxUI, std::__Cr::default_delete<views::LinuxUI> >))
    
    ld.lld: error: undefined symbol: ui::ShellDialogLinux::ShellDialogLinux()
    >>> referenced by linux_ui.cc:38 (../../ui/views/linux_ui/linux_ui.cc:38)
    >>>               obj/ui/views/views/linux_ui.o:(views::LinuxUI::LinuxUI())
    
    ld.lld: error: undefined symbol: ui::ShellDialogLinux::~ShellDialogLinux()
    >>> referenced by linux_ui.cc:40 (../../ui/views/linux_ui/linux_ui.cc:40)
    >>>               obj/ui/views/views/linux_ui.o:(views::LinuxUI::~LinuxUI())
    ```
    
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- ui/views/linux_ui/linux_ui.cc
+++ ui/views/linux_ui/linux_ui.cc
@@ -21,7 +21,7 @@ void LinuxUI::SetInstance(std::unique_ptr<LinuxUI> instance) {
   g_linux_ui = instance.release();
 
   SkiaFontDelegate::SetInstance(g_linux_ui);
-#if BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_CHROMECAST)
+#if BUILDFLAG(IS_LINUX) && defined(USE_GTK) && !BUILDFLAG(IS_CHROMECAST)
   ShellDialogLinux::SetInstance(g_linux_ui);
 #endif
   ui::SetTextEditKeyBindingsDelegate(g_linux_ui);
