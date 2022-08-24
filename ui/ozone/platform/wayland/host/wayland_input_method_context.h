0375fc619857a8b90292b3bbd88ad105c75849fc
Takuro Ashie <ashie@clear-code.com>
2022-08-24 09:50:28 +0900

    [ozone/wayland] Enable to set content type to input method
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- ui/ozone/platform/wayland/host/wayland_input_method_context.h
+++ ui/ozone/platform/wayland/host/wayland_input_method_context.h
@@ -46,6 +46,7 @@ class WaylandInputMethodContext : public LinuxInputMethodContext,
   void SetCursorLocation(const gfx::Rect& rect) override;
   void SetSurroundingText(const std::u16string& text,
                           const gfx::Range& selection_range) override;
+  void SetTextInputType(TextInputType text_input_type) override;
   void Reset() override;
   void Focus() override;
   void Blur() override;
--- ui/ozone/platform/wayland/host/zwp_text_input_wrapper.h
+++ ui/ozone/platform/wayland/host/zwp_text_input_wrapper.h
@@ -86,6 +86,7 @@ class ZWPTextInputWrapper {
   virtual void SetCursorRect(const gfx::Rect& rect) = 0;
   virtual void SetSurroundingText(const std::string& text,
                                   const gfx::Range& selection_range) = 0;
+  virtual void SetContentType(uint32_t hint, uint32_t purpose) = 0;
 };
 
 }  // namespace ui
